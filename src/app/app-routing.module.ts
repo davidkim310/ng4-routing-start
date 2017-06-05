b2c9141456fa12c005a8008e6408be4a05fc140e
davidkim310 <davidkim310@gmail.com>
2017-06-04 21:20:56 -0700

    passes static data to 404 page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,8 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ErrorPageComponent } from './error-page/error-page.component';
+
 import { Routes, RouterModule } from '@angular/router';
 import { AuthGuard } from './auth-guard.service';
 import { CanDeactivateGuard } from './servers/edit-server/can-deactivate-guard.service';
@@ -27,7 +29,8 @@ const appRoutes: Routes = [
     { path: ':id/edit', component: EditServerComponent, canDeactivate: [CanDeactivateGuard]}
   ] },
   //404 page
-  { path: 'not-found', component: PageNotFoundComponent },
+  // { path: 'not-found', component: PageNotFoundComponent },
+  { path: 'not-found', component: ErrorPageComponent, data: {message: 'Page not found!'} },
   //alternative to component uses redirect
   //wildcard route: make sure it is the last route
   { path: '**', redirectTo: '/not-found' } 
